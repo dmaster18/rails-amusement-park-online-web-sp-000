3b5a016ecefabb92046ff68b45ff5bb1a761b154
David Strauch <dmaster18@gmail.com>
2021-01-26 00:51:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :verified_user
   helper_method :current_user
 
+  private
+
+  def verified_user
+    redirect_to '/' unless user_is_authenticated
+  end
+
+  def user_is_authenticated
+    !!current_user
+  end
+
   def current_user
-    current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  def logged_in?
-    !!session[:user_id]
-  end
-
-  def log_in_required
-    if !logged_in?
-      redirect_to root_path
-    end
+    User.find_by(id: session[:user_id])
   end
 end
