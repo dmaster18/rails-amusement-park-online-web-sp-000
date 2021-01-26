3f4b9e3d5d94e8a0dc0b107e4555e9d65c29afca
David Strauch <dmaster18@gmail.com>
2021-01-26 00:53:27 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,21 @@
 class SessionsController < ApplicationController
-    def signup
-        
-    end
+  skip_before_action :verified_user, only: [:new, :create]
 
-    def create
-      @user = User.find_by(id: params[:user_name])
-      if @user
-        if @user.authenticate(params[:user_password]) || @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect_to user_path(@user)
-        else
-          redirect_to root_path
-        end
-      else
-        redirect_to root_path
-      end
-    end
+  def new
+    @user = User.new
+  end
 
-    def destroy
-      session.clear
-      redirect_to root_path
+  def create
+    if @user = User.find_by(name: params[:user][:name])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render 'new'
     end
+  end
+
+  def destroy
+    session.delete("user_id")
+    redirect_to root_path
+  end
 end
