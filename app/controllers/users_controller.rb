e0e535e34002516613a64df971a33bc79589090c
David Strauch <dmaster18@gmail.com>
2021-01-26 00:54:22 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,34 @@
 class UsersController < ApplicationController
+  skip_before_action :verified_user, only: [:new, :create]
 
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    if (user = User.create(user_params))
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      render 'new'
     end
+  end
 
-    def create
-        @user = User.create(user_params)
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
-    end
+  def show
+    @user = User.find_by(id: params[:id])
+  end
 
-    def show
-        log_in_required
-        @user = User.find_by(id: params[:id])
-    end
+  private
 
-
-
-    private
-
-    def user_params
-        params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :admin, :password)
-    end
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :height,
+      :nausea,
+      :tickets,
+      :admin,
+      :password,
+      :happiness
+    )
+  end
 end
